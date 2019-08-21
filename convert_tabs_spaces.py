79d3eb7807c061a32003eb3740271cc0a1f32941
Thomas Sablik <thomas.sablik@wombytes.de>
2019-08-21 13:46:29 +0200

    Fix pylint warnings

--- convert_tabs_spaces.py
+++ convert_tabs_spaces.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Converts tabs into spaces
+'''
 
 import argparse
 import os.path
@@ -7,6 +12,9 @@ import sys
 __version__ = '0.0.1'
 
 def parse_arguments():
+    '''
+    Parse commandline arguments
+    '''
     parser = argparse.ArgumentParser(description='Replace tabs with spaces')
     parser.add_argument('files', metavar='FILE', help='files', nargs='+')
     parser.add_argument('-t', '--tabwidth', metavar='TABWIDTH', dest='tabwidth', default=4,
@@ -17,31 +25,39 @@ def parse_arguments():
 
     return args.files, args.tabwidth
 
-def convertLine(line, tabwidth):
+def convert_line(line, tabwidth):
+    '''
+    Converts all tabs to spaces in line
+    '''
     pos = line.find('\t')
     while pos != -1:
-        tabwidth - (pos % tabwidth)
         line = line.replace('\t', (tabwidth - (pos % tabwidth)) * ' ', 1)
         pos = line.find('\t', pos)
     return line
-    
+
 
 def main():
+    '''
+    Main function
+    Parse arguments
+    Checks given paths
+    Converts tabs to spaces in all files
+    '''
     files, tabwidth = parse_arguments()
 
-    for f in files:
-        if os.path.isdir(f):
-            print('Path is a directory: ' + f, file=sys.stderr)
+    for current_file_path in files:
+        if os.path.isdir(current_file_path):
+            print('Path is a directory: ' + current_file_path, file=sys.stderr)
             continue
 
-        if not os.path.isfile(f):
+        if not os.path.isfile(current_file_path):
         #    print('File not found: ' + f, file=sys.stderr)
             continue
 
         converted = ''
-        with open(f, 'r') as file:
-            for line in file:
-                converted += convertLine(line, tabwidth)
+        with open(current_file_path, 'r') as current_file:
+            for line in current_file:
+                converted += convert_line(line, tabwidth)
         print(converted)
         # with open(filename, 'w') as file:
         #     file.write(converted)
